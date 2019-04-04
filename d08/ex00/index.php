56f0b728bf1feafdf7a27b04baf600400592a45c
Joseph Kaplin <kaplinjoseph@gmail.com>
2019-04-04 13:11:27 -0700

    P

---
+++ d08/ex00/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+    </header>
+    <main>
+      <div id="grid">
+        <?php for ($i = 0; $i < $_SERVER["grid-height"]; $i++): ?>
+          <div class="box" id="x <?php echo $i ?>
+      </div>
+    </main>
+  </body>
+</html>
